814119974ef54dadc641cfdf010454fe67952b1a
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2021-11-03 11:41:56 +0100

    Update and rename install_terraform.sh to vagrant_scripts/install_terraform.sh

--- vagrant_scripts/install_terraform.sh
+++ vagrant_scripts/install_terraform.sh
@@ -2,9 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# install terraform
+# configure hashicorp apt repo
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
+# install terraform
+# we will use version 0.11.15 as its the version used in the tutorial
 apt-get update -y
 apt-get install -y terraform=0.11.15
